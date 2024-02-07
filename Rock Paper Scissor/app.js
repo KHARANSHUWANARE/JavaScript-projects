5845a4292956737beb802c26761a961a75aea83e
Kharanshu <kharanshuw@gmail.com>
2024-02-07 16:30:48 +0530

    game completed

--- Rock Paper Scissor/app.js
+++ Rock Paper Scissor/app.js
@@ -5,31 +5,44 @@ let msg=document.querySelector("#msg");
 let user=document.querySelector("#userchoice");
 let comp=document.querySelector("#compchoice");
 
+const userscorepara=document.querySelector("#user-score");
+const compscorepara=document.querySelector("#comp-score");
+
+
+
 const gencompchoice = () => {
     const options = ["rock", "paper", "scissors"];
     const randidx = Math.floor(Math.random() * 3);
     return options[randidx];
 }
-let drawgame = () => {
+let drawgame = (userchoice,compchoice) => {
     console.log("draw game");
+    user.style.visibility = "visible";
+   comp.style.visibility = "visible";
+   user.textContent="User Choice: "+userchoice;
+   comp.textContent="Computer Choice: "+compchoice;
     msg.innerHTML="Game Draw,Play again";
     msg.style.backgroundColor="#081B31";
 }
 
 let showwinner=(userwin,userchoice,compchoice)=>{
-   
+  
    user.style.visibility = "visible";
    comp.style.visibility = "visible";
    user.textContent="User Choice: "+userchoice;
    comp.textContent="Computer Choice: "+compchoice;
     if(userwin)
     {
+        userscore++;
+        userscorepara.innerHTML=userscore;
         console.log("user won");
         msg.innerHTML="User Won";
         msg.style.backgroundColor="green";
    }
     else 
     {
+        compscore++;
+        compscorepara.innerHTML=compscore;
         console.log("computer won");
         msg.innerHTML="Computer Won,User Lost";
         msg.style.backgroundColor="red";
@@ -42,7 +55,7 @@ const playgame = (userchoice) => {
     console.log("compchoice", compchoice);
     comp.innerHTML=compchoice;
     if (userchoice === compchoice) {
-        drawgame();
+        drawgame(userchoice,compchoice);
     }
     else
     {
